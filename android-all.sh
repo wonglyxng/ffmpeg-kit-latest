4496b83067b99560ffc680fa45a5c4ea9d636594
wonlyxng <wspoys@gmail.com>
2025-03-23 11:30:41 +0800

    test update

--- android-all.sh
+++ android-all.sh
@@ -4,5 +4,5 @@ set -euxo pipefail
 
 
 ./android.sh --lts --enable-android-media-codec --enable-android-zlib --disable-arm-v7a --disable-arm-v7a-neon --disable-x86 --disable-x86-64 \
-  --enable-{libiconv}
+  --enable-iconv
 mv prebuilt/bundle-android-aar-lts/ffmpeg-kit/ffmpeg-kit.aar ffmpeg-kit-https.aar
